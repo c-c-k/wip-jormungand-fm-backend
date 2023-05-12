a71083b39f9a452e0e7af0bc41a147896ddf682f
c-c-k <c-c-k@nym.hush.com>
2023-05-12 18:06:10 +0300

    connected dynaconf with app and pytest

---
+++ jormungand/__init__.py
@@ -0,0 +1,8 @@
+from pprint import pp
+from config import settings
+
+
+def main():
+    # pp(settings.as_dict(internal=True))
+    pass
+
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,8 @@
+from jormungand import settings
 import pytest
+
+
+@pytest.fixture(scope="session", autouse=True)
+def set_test_settings():
+    settings.configure(FORCE_ENV_FOR_DYNACONF="testing")
+
