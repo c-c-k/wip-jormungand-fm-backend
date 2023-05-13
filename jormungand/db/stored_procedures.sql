6c221d412c038852921e17e19df9dad9d3e63bd1
c-c-k <c-c-k@nym.hush.com>
2023-05-13 22:39:52 +0300

    added postgresql schema and stored procedures
    Squashed commit of the following:
    
    commit c901f9987e91768a2c07543edd9a1399b90d7c4b
    Author: c-c-k <c-c-k@nym.hush.com>
    Date:   Wed May 10 21:29:29 2023 +0300
    
        created stored_procedures.sql
    
    commit 36f7df7af2bbf3e34061b524a0e01b2be4a1fa06
    Author: c-c-k <c-c-k@nym.hush.com>
    Date:   Wed May 10 20:23:08 2023 +0300
    
        created schema.sql

---
+++ jormungand/db/stored_procedures.sql
@@ -0,0 +1,74 @@
+CREATE OR REPLACE PROCEDURE get_user_by_username(_username text)
+    --Returns basic user info (*Users* table only), this should be used during user login.
+LANGUAGE SQL
+AS $$
+    SELECT * FROM users WHERE username = _username;
+$$;
+
+CREATE OR REPLACE PROCEDURE get_airline_by_username(_username text)
+    --Returns full airline info (join of *Airline_Companies* and *Users* tables).
+LANGUAGE SQL
+AS $$
+    SELECT * FROM users
+        INNER JOIN airline_companies ON (users.id = airline_companies.user_id)
+    WHERE username = _username;
+$$;
+
+CREATE OR REPLACE PROCEDURE get_customer_by_username(_username text)
+    --Returns full customer info (join of *Customers* and *Users* tables).
+LANGUAGE SQL
+AS $$
+SELECT * FROM users
+                  INNER JOIN customers ON (users.id = customers.user_id)
+WHERE username = _username;
+$$;
+
+CREATE OR REPLACE PROCEDURE get_flights_by_parameters(
+        _origin_country_id int, _destination_country_id int, _date date)
+    --Returns a list of all flights answering the input parameters of origin, destination and date.
+LANGUAGE SQL
+AS $$
+    SELECT * FROM flights
+    WHERE
+        (origin_country_id = _origin_country_id)
+        AND (destination_country_id = _destination_country_id)
+        AND (departure_time = _date)
+$$;
+
+CREATE OR REPLACE PROCEDURE get_flights_by_airline_id(_airline_id bigint)
+    --Returns a list of all flights belonging to airline company of the input id.
+LANGUAGE SQL
+AS $$
+SELECT * FROM flights WHERE airline_id = _airline_id
+$$;
+
+CREATE OR REPLACE PROCEDURE get_arrival_flights(_country_id int)
+    --Returns a list of all flights scheduled to land in the country of the input id in the next 12 hours.
+LANGUAGE SQL
+AS $$
+SELECT * FROM flights
+WHERE
+  (destination_country_id = _country_id)
+  AND (landing_time >= current_timestamp)
+  AND (landing_time <= (current_timestamp + make_interval(hours=>12)))
+$$;
+
+CREATE OR REPLACE PROCEDURE get_departure_flights(_country_id int)
+    --Returns a list of all flights scheduled to take off from the country of the input id in the next 12 hours.
+LANGUAGE SQL
+AS $$
+SELECT * FROM flights
+WHERE
+    (origin_country_id = _country_id)
+  AND (landing_time >= current_timestamp)
+  AND (landing_time <= (current_timestamp + make_interval(hours=>12)))
+$$;
+
+CREATE OR REPLACE PROCEDURE get_tickets_by_customer(_customer_id bigint)
+    --Returns a list of all the tickets purchased by the customer of the input id.
+LANGUAGE SQL
+AS $$
+    SELECT * FROM tickets WHERE customer_id = _customer_id
+$$;
+
+
