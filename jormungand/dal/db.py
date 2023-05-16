01c2ed943e7d2972429c074c355c7780bddc78aa
c-c-k <c-c-k@nym.hush.com>
2023-05-16 21:04:11 +0300

    added basic sqlalchemy/psycopg2 db connection
    Squashed commit of the following:
    
    commit b60d12b93b5a0dc96c5818c57134f6612df6b35c
    Author: c-c-k <c-c-k@nym.hush.com>
    Date:   Tue May 16 21:00:49 2023 +0300
    
        added database cleanup and fixture to conftest.py
    
    commit 446089c5dd7f685cd5ecc4b9378aaadd84e54c37
    Author: c-c-k <c-c-k@nym.hush.com>
    Date:   Tue May 16 21:00:22 2023 +0300
    
        adjusted test_db to refactor
    
    commit ef7717883808ec7ba634616abd2a4e4d96577683
    Author: c-c-k <c-c-k@nym.hush.com>
    Date:   Tue May 16 20:59:48 2023 +0300
    
        minor fixes
    
    commit c1b462d4a00f492ea949ab17b3e1c2bd3676426c
    Author: c-c-k <c-c-k@nym.hush.com>
    Date:   Tue May 16 20:59:19 2023 +0300
    
        added .secrets.yaml example
    
    commit 25429a5954123094c42c9d72ea27177ce80a9881
    Author: c-c-k <c-c-k@nym.hush.com>
    Date:   Tue May 16 20:58:48 2023 +0300
    
        refactored and added init_db to dal/db
    
    commit f6504c65a19328ba37ec5b2386fd9daf42a50e5f
    Author: c-c-k <c-c-k@nym.hush.com>
    Date:   Mon May 15 13:15:44 2023 +0300
    
        added get_engine
    
    commit ba4723da2962b7dfbb20292e881f56a6600231ae
    Author: c-c-k <c-c-k@nym.hush.com>
    Date:   Mon May 15 12:36:40 2023 +0300
    
        refactored db/ dir into dal/ package
    
    commit 26d6227b7714aee00fb069783a2771cb95e65b70
    Author: c-c-k <c-c-k@nym.hush.com>
    Date:   Mon May 15 09:12:58 2023 +0300
    
        installed sqlalchemy+psycopg2

---
+++ jormungand/dal/db.py
@@ -0,0 +1,40 @@
+"""db connection and access functions
+
+"""
+from pathlib import Path
+
+from sqlalchemy import create_engine, text, Engine
+from sqlalchemy.engine import URL
+
+from config import settings
+from jormungand.logging_manager import LoggingManager
+
+logger = LoggingManager.get_logger(__name__)
+
+_CURRENT_DIR = Path(__file__).parent
+
+_engine: Engine | None = None
+
+
+def _init_engine():
+    global _engine
+    _engine = create_engine(
+            URL.create(**settings['database']),
+            echo=settings.get('db_echo', False))
+
+
+def get_db_engine() -> Engine:
+    if _engine is None:
+        _init_engine()
+    return _engine
+
+def init_db():
+    with get_db_engine().connect() as conn:
+        with _CURRENT_DIR.joinpath('schema.sql').open() as f:
+            query = text(f.read())
+            conn.execute(query)
+        with _CURRENT_DIR.joinpath('stored_procedures.sql').open() as f:
+            query = text(f.read())
+            conn.execute(query)
+        conn.commit()
+    
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,54 @@
 import pytest
 
-from jormungand import settings
+from sqlalchemy import create_engine, text
+from sqlalchemy.engine import URL
+
+from config import settings
 from jormungand.logging_manager import LoggingManager
+from jormungand.dal.db import get_db_engine, init_db
+
+_cleanup_db_engine = None
+
+# .. note::
+#       the following doesn't work because postgresql doesn't 
+#       allow dropping a database from within a stored procedure.
+#
+# CLEANUP_DB_STORED_PROCEDURE = """
+# CREATE OR REPLACE PROCEDURE cleanup_test_db(_test_db_name text)
+# LANGUAGE SQL
+# AS $$
+#     DROP DATABASE IF EXISTS _test_db_name (FORCE);
+#     CREATE DATABASE _test_db_name;
+# $$;
+# """.strip()
+
+
+def _init_cleanup_db_engine():
+    global _cleanup_db_engine
+    _cleanup_db_engine = create_engine(
+            URL.create(**settings['cleanup_database']),
+            isolation_level='AUTOCOMMIT')
+# .. note::
+#       the following doesn't work because postgresql doesn't 
+#       allow dropping a database from within a stored procedure.
+#
+    # with _cleanup_db_engine.connect() as conn:
+    #     query = text(CLEANUP_DB_STORED_PROCEDURE)
+    #     conn.execute(query)
+    #     conn.commit()
+
+
+def _cleanup_test_db(test_db_name):
+    if _cleanup_db_engine is None:
+        _init_cleanup_db_engine()
+    with _cleanup_db_engine.begin() as conn:
+        # TODO: add some sort of manual counter sql injection 
+        #       validation to test_db_name
+        conn.execute(
+                text(f'DROP DATABASE IF EXISTS {test_db_name} (FORCE);'),
+                )
+        conn.execute(text(f'CREATE DATABASE {test_db_name};'))
+        conn.commit()
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -13,4 +60,12 @@ def set_test_settings():
 def set_test_logging(set_test_settings):
     """Set global logging configuration for tests"""
     LoggingManager().setup_root_logger()
+
+
+@pytest.fixture
+def db(set_test_settings):
+    _cleanup_test_db(settings['database.database'])
+    init_db()
+    yield get_db_engine()
+
 
