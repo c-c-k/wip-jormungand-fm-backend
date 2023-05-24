c31645ca9ae57fb5d4745fff5a85dc306c4a5adf
c-c-k <c-c-k@nym.hush.com>
2023-05-24 16:06:47 +0300

    removed singleton use from project

--- jormungand/utils/singleton.py
+++ jormungand/utils/singleton.py
@@ -1,4 +1,14 @@
 """Singleton implementation/s.
+
+This is implementation is here is here because 
+the original project requirement required it
+and because singletons and metaclasses sound cool.
+
+Nevertheless it is not used in the project because:
+    1. Python modules are already essentially singletons.
+    2. Using global module level variables is as much a python 
+       antipattern as using a singleton is.
+    3. Modules are easier to mock during unittesting.
 """
 
 class Singleton(type):
