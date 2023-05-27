dde69eb89471ce78233c847b4cf9bfb191b02c15
c-c-k <c-c-k@nym.hush.com>
2023-05-27 19:49:35 +0300

    unsorted mess

--- jormungand/dal/base.py
+++ jormungand/dal/base.py
@@ -5,10 +5,15 @@ TODO: dal.base module docstring
 from enum import IntEnum
 from pathlib import Path
 
-from sqlalchemy import text, MetaData, Table
+from sqlalchemy import (
+    text, MetaData, Table, insert)
 
 from jormungand.core.logging import get_logger
 from jormungand.core.db import get_db_connection, get_db_engine
 
 logger = get_logger(__name__)
 
+
+def insert_(table_name, entries):
+    with get_db_connection() as conn:
+        conn.execute(insert(table_name), entries)
