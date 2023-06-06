539fd32314e55a3dd357d36dbfb8dcc8000d05a3
c-c-k <c-c-k@nym.hush.com>
2023-06-06 19:44:28 +0300

    unsorted mess

--- jormungand/dal/base.py
+++ jormungand/dal/base.py
@@ -1,11 +1,12 @@
 """
 TODO: dal.user module docstring
 """
+import re
 
 from sqlalchemy import (
         Connection, Table, select, insert,
         update as sa_update, delete as sa_delete)
-from sqlalchemy.exc import NoResultFound, IntegrityError, DataError
+from sqlalchemy.exc import NoResultFound, IntegrityError, SQLAlchemyError
 
 from jormungand.core import db
 from jormungand.core.exceptions import (
@@ -34,22 +35,73 @@ def get_all(table: str) -> list[dict]:
         result = conn.execute(stmt).mappings().all()
         return list(dict(mapping) for mapping in result)
 
+# TODO: STOPPED AT: trying to decide if get operations should return
+#       data/raise exception like they do now or return status dict
+#       like I'm thinking to do with the CUD operations.
+
+def _gen_unexpected_err_info(error: Exception, **kwargs) -> dict:
+    return {
+            "status": "UnexpectedError",
+            "info": {
+                "error_type": error.__class__,
+                "error_message": error.args[0],
+                **kwargs
+                }
+            }
+
+
+def _gen_duplication_err_info(error: Exception, table: Table) -> dict:
+    column_name, value = re.search(
+            r"DETAIL:  Key \(([^)+])\)=\(([^)+])\) already exists."
+            ).groups
+    return {
+            "status": "DuplicationError",
+            "info": {
+                "table": table.name,
+                "column": column_name,
+                "value": value
+                }
+            }
+
 
 def _insert_one(
         conn: Connection, table: Table, id_c_name: str, data: dict
         ) -> dict:
     stmt = insert(table).values(data).returning(table)
     try:
-        result = conn.execute(stmt, data).mappings().one()
-    except (IntegrityError, DataError) as e:
-        raise e
-        if "duplicate key" in e.args[0]:
-            raise DuplicateKeyError(table_name=table.name,
-                                    column_name=id_c_name,
-                                    value=data[id_c_name])
+        result = {
+                "status": "success",
+                "data": conn.execute(stmt, data).mappings().one()
+                }
+    except IntegrityError as e:
+        if "duplicate key value violates unique" in e.args[0]:
+            result = _gen_duplication_err_info(
+                    err_msg=e.args[0], table_name=table.name,
+                    data=data, action="insert")
         else:
-            raise InvalidDataError(e.args[0])
-    return dict(result)
+            result = _gen_unexpected_err_info(
+                    err_msg=e.args[0], table_name=table.name, data=data)
+    except SQLAlchemyError:
+        result = _gen_unexpected_err_info(
+                err_msg=e.args[0], table_name=table.name, data=data)
+    return result
+
+
+# def _insert_one(
+#         conn: Connection, table: Table, id_c_name: str, data: dict
+#         ) -> dict:
+#     stmt = insert(table).values(data).returning(table)
+#     try:
+#         result = conn.execute(stmt, data).mappings().one()
+#     except (IntegrityError, DataError) as e:
+#         raise e
+#         if "duplicate key" in e.args[0]:
+#             raise DuplicateKeyError(table_name=table.name,
+#                                     column_name=id_c_name,
+#                                     value=data[id_c_name])
+#         else:
+#             raise InvalidDataError(e.args[0])
+#     return dict(result)
 
 
 def add_one(table: str | Table, id_c_name: str, data: dict) -> dict:
