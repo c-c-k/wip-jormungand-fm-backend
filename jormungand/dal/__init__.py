30dc73758bc2a424a4bef64848cd939b746b164c
c-c-k <c-c-k@nym.hush.com>
2023-06-16 22:50:04 +0300

    dal.airports: added get airports by substring

--- jormungand/dal/__init__.py
+++ jormungand/dal/__init__.py
@@ -1,4 +1,5 @@
 from .airports import airports_init_data
+from .airports import get_airports_by_substring
 from .countries import countries_get_code_to_id_map
 from .countries import countries_init_data
 from .ourairports import oa_airports_get_all
--- jormungand/dal/airports.py
+++ jormungand/dal/airports.py
@@ -2,6 +2,8 @@
 
 """
 
+import sqlalchemy as sa
+
 from . import base
 from jormungand.core import db
 from jormungand.core.logging import get_logger
@@ -19,3 +21,27 @@ def airports_init_data(data: list[dict]):
     :returns: None
     """
     base.init_table_data(_TABLE_NAME, data)
+
+
+def get_airports_by_substring(substring: str, limit: int = 5) -> list[dict]:
+    """Get a small selection of airports matching a substring.
+
+    The substring is matched against:
+
+    1. Airport IATA code
+    2. Airport country code
+    3. Airport country name
+    4. Airport principality
+    5. Airport airport name
+
+    :substring: The substring to match against the above fields.
+    :limit: The number of results to return.
+    :returns: A short list of airport matches.
+    """
+    with db.get_db_connection() as conn:
+        result = conn.execute(sa.text(
+            """
+            SELECT * FROM GET_AIRPORTS_BY_SUBSTRING(:substring, :limit)
+            """), {"substring": substring, "limit": limit}
+            ).mappings().all()
+        return [dict(entry) for entry in result]
