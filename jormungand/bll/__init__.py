45af5f08c5da6d414738f406634a04c086310cf2
c-c-k <c-c-k@nym.hush.com>
2023-07-01 16:11:27 +0300

    temp

--- jormungand/bll/__init__.py
+++ jormungand/bll/__init__.py
@@ -1,2 +1,7 @@
+"""Wraper for Business Logic Layer functionalities. 
+
+See the induvidial modules for specific documentation
+"""
 from .ourairports import import_oa_country_data
 from .ourairports import import_oa_airport_data
+from .airports import find_airports
